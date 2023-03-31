0450f84168e65b9e00f2c636fec37be2933d82a9
GadzZzilo <GadzZzilo@yandex.ru>
2023-03-31 22:21:36 +0300

    продолжение

--- sss/config/settings.py
+++ sss/config/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
